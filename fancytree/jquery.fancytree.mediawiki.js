29cbcd505f885078f1577470da295fe6fec63b62
Aran <aran@organicdesign.co.nz>
2015-06-15 13:07:15 -0300

    debugging makeTitleVisible

--- fancytree/jquery.fancytree.mediawiki.js
+++ fancytree/jquery.fancytree.mediawiki.js
@@ -22,7 +22,7 @@
 		if(typeof(title) === 'undefined') title = mw.config.get('wgTitle');
 		this.visit(function(node) {
 			var x = $('a', $(node.title)).attr('title');
-			console.log(x);
+			console.log('title: ' + x);
 			if($(node.title) == title) {
 				node.makeVisible({ noAnimation: true, noEvents: true, scrollIntoView: false });
 				node.setActive({ noEvents: true });
