ea9ae4ea6f241f84dcce6e2211bdc4232178aebd
Aran <aran@organicdesign.co.nz>
2015-06-22 11:06:57 -0300

    update legacy entry point errors

--- TreeAndMenu.php
+++ TreeAndMenu.php
@@ -1,17 +1,14 @@
 <?php
 if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'FooBar' );
-	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	$wgMessagesDirs['FooBar'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['FooBar'] = __DIR__ . '/FooBar.alias.php';
-	wfWarn(
-		'Deprecated PHP entry point used for FooBar extension. Please use wfLoadExtension instead, ' .
-		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	$ext = preg_replace( '%^.+/(.+?)\.php$%', '$1', __FILE__ );
+	die(
+		"Deprecated PHP entry point used for <i>$ext</i> extension. Please use wfLoadExtension instead, " .
+		'see <a href="https://www.mediawiki.org/wiki/Extension_registration">Extension registration</a> for more details.'
 	);
 	return;
 } else {
 	die(
-		'<b>Fatal error:</b> This version of the FooBar extension requires MediaWiki 1.25+, ' .
+		"<b>Fatal error:</b> This version of the <i>$ext</i> extension requires MediaWiki 1.25+, " .
 		'either <a href="https://mediawiki.org/wiki/Download">upgrade your MediaWiki</a> or download the extension code from the ' .
 		'<a href="https://github.com/OrganicDesign/extensions/tree/MediaWiki-1.24/MediaWiki">1.24 branch</a>.'
 	);
